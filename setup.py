2bb0641f367da4edbc455aa62eb0eb54166224d5
Silas Bischoff <silas.bischoff@brockhaus-ag.de>
2018-05-29 12:27:09 +0200

    defined setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name='PyGaSe',
@@ -9,7 +9,23 @@ setup(
     author='Silas Bischoff',
     author_email='silas.bischoff@stud.uni-due.de',
     url='https://github.com/sbischoff-ai/python-game-service',
-    license='MIT',
+    download_url= 'https://github.com/sbischoff-ai/python-game-service/archive/0.1.tar.gz',
+    keywords=['server', 'client', 'games', 'gaming', 'real-time', 'network', 'UDP', 'protocol', 'game server', 'game service'],
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Topic :: Games/Entertainment',
+        'Topic :: Internet',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha'
+    ],
     packages=['PyGaSe'],
     install_requires=['umsgpack']
 )
