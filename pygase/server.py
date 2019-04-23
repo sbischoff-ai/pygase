f2faf60f002e55906af88e87cd0d972cff50205e
Bischoff, Silas <SBischoff@brockhaus-ag.de>
2019-04-23 08:35:40 +0200

    Add docstrings to server module

--- pygase/server.py
+++ pygase/server.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+'''
+Provides the **Server** class.
+'''
+
 import threading
 
 import curio
@@ -10,7 +14,22 @@ from pygase.gamestate import GameStateStore, GameStateMachine
 from pygase.event import UniversalEventHandler, Event
 
 class Server:
+    '''
+    Part of the PyGaSe backend that handles connections to clients.
+
+    ### Arguments
+     - **game_state_store** *GameStateStore*: part of the backend that provides the **GameState**
     
+    ### Attributes
+     - **connections** *dict*: contains each clients address as a key leading to the
+       corresponding **ClientConnection** instance
+     - **host_client**: address of the host client, if there is any
+     - **game_state_store** *GameStateStore*: part of the backend that provides the **GameState**
+    
+    ### Properties
+     - **hostname** *str*: read-only access to the servers hostname
+     - **port** *int*: read-only access to the servers port number
+    '''
     def __init__(self, game_state_store:GameStateStore):
         self.connections = {}
         self.host_client = None
@@ -21,13 +40,15 @@ class Server:
 
     def run(self, port:int=0, hostname:str='localhost', event_wire=None):
         '''
-        Starts the server under specified address.
+        Starts the server under specified address. (Can also be called as a coroutine.)
 
         ### Arguments
          - **port** *int*: port number the server will be bound to, default will be an available
            port chosen by the computers network controller
          - **hostname** *str*: hostname or IP address the server will be bound to.
            Defaults to `'localhost'`.
+         - **event_wire**: object to which events are to be repeated
+           (has to implement a *_push_event* method and is typically a **GameStateMachine**)
         '''
         curio.run(self.run, port, hostname, event_wire)
 
@@ -71,7 +92,19 @@ class Server:
 
     def dispatch_event(self, event_type:str, handler_args:list=[], target_client='all', retries:int=0, ack_callback=None, **kwargs):
         '''
-        ack_callback will be passed a reference to the client connection
+        Sends an event to one or all clients.
+
+        ### Arguments
+         - **event_type** *str*: string that identifies the event and links it to a handler
+        
+        ### Optional Arguments
+         - **handler_args** *list*: list of positional arguments to be passed to the handler function that will be invoked
+           by the client
+         - **target_client**: either `'all'` for an event broadcast or a clients address
+         - **retries** *int*: number of times the event is to be resent, in the case it times out
+         - **ack_callback**: function or coroutine to be executed after the event was received,
+           will be passed a reference to the client connection
+         - **kwargs** *dict*: keyword arguments to be passed to the handler function
         '''
         event = Event(event_type, handler_args, kwargs)
         def get_ack_callback(connection): return lambda: ack_callback(connection)
@@ -102,4 +135,9 @@ class Server:
             )
 
     def push_event_handler(self, event_type:str, handler_func):
+        '''
+        ### Arguments
+         - **event_type** *str*: event type to link the handler function to
+         - **handler_func**: function or coroutine to be invoked for events of the given type
+        '''
         self._universal_event_handler.push_event_handler(event_type, handler_func)
