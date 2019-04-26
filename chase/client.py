4f8de28472dd6316191a67ba105a42443fd46c74
Bischoff, Silas <SBischoff@brockhaus-ag.de>
2019-04-26 18:08:35 +0200

    Fix Chase

--- chase/client.py
+++ chase/client.py
@@ -1,11 +1,10 @@
-'''
-Example game client
-'''
+"""Example game client"""
 
 from time import sleep
 import pygame
 import pygame.locals
 from pygase import Client
+
 
 class ChaseGame:
     client = Client()
@@ -16,13 +15,10 @@ class ChaseGame:
 
     @classmethod
     def init(cls):
-        '''Connect the client and join a new player to the server.'''
-        cls.client.connect_in_thread(hostname='localhost', port=8080)
-        cls.local_player_name = input('Player name: ')
-        cls.client.dispatch_event(
-            event_type='JOIN',
-            handler_args=[cls.local_player_name]
-        )
+        """Connect the client and join a new player to the server."""
+        cls.client.connect_in_thread(hostname="localhost", port=8080)
+        cls.local_player_name = input("Player name: ")
+        cls.client.dispatch_event("JOIN", cls.local_player_name)
 
     @classmethod
     def set_player_id(cls):
@@ -30,14 +26,12 @@ class ChaseGame:
         while cls.local_player_id is None:
             with cls.client.access_game_state() as game_state:
                 for player_id, player in game_state.players.items():
-                    if player['name'] == cls.local_player_name:
+                    if player["name"] == cls.local_player_name:
                         cls.local_player_id = player_id
 
     @classmethod
     def game_loop(cls):
-        '''
-        Render frames
-        '''
+        """Render frames."""
         # Initialize a pygame screen
         pygame.init()
         screen_width = 640
@@ -52,7 +46,7 @@ class ChaseGame:
                 for player_id, player in game_state.players.items():
                     is_chaser = player_id == game_state.chaser_id
                     is_self = player_id == cls.local_player_id
-                    cls.draw_player(screen, player['position'], is_chaser, is_self)
+                    cls.draw_player(screen, player["position"], is_chaser, is_self)
             # Handle events
             for event in pygame.event.get():
                 if event.type == pygame.locals.QUIT:
@@ -64,28 +58,25 @@ class ChaseGame:
             # Handle player movement
             dx, dy = 0, 0
             if pygame.locals.K_DOWN in cls.keys_pressed:
-                dy += int(0.1*dt)
+                dy += int(0.1 * dt)
             elif pygame.locals.K_UP in cls.keys_pressed:
-                dy -= int(0.1*dt)
+                dy -= int(0.1 * dt)
             elif pygame.locals.K_RIGHT in cls.keys_pressed:
-                dx += int(0.1*dt)
+                dx += int(0.1 * dt)
             elif pygame.locals.K_LEFT in cls.keys_pressed:
-                dx -= int(0.1*dt)
+                dx -= int(0.1 * dt)
             # Create a client activity for the player's movement
             with cls.client.access_game_state() as game_state:
-                old_position = game_state.players[cls.local_player_id]['position']
+                old_position = game_state.players[cls.local_player_id]["position"]
                 cls.client.dispatch_event(
-                    event_type='MOVE',
+                    event_type="MOVE",
                     player_id=cls.local_player_id,
-                    new_position=(
-                        (old_position[0] + dx) % screen_width,
-                        (old_position[1] + dy) % screen_height
-                    )
+                    new_position=((old_position[0] + dx) % screen_width, (old_position[1] + dy) % screen_height),
                 )
             # Do the thing.
             pygame.display.flip()
         pygame.quit()
-    
+
     @classmethod
     def cleanup(cls):
         # You need to disconnect from the server
@@ -93,17 +84,19 @@ class ChaseGame:
 
     @staticmethod
     def draw_player(screen, position, is_chaser, is_self):
-        '''
-        Little function to draw players as colored circles
+        """Draw players as colored circles.
+
          - Green: Yourself
          - Blue: Others
          - Red: The chaser
-        '''
+
+        """
         if is_self:
             color = (50, 255, 50)
         else:
             color = (255, 50, 50) if is_chaser else (50, 50, 255)
         pygame.draw.circle(screen, color, position, 10)
+
 
 ChaseGame.init()
 ChaseGame.set_player_id()
--- chase/server.py
+++ chase/server.py
@@ -2,36 +2,28 @@ import sys
 from pygase import Server, GameState, GameStateStore, GameStateMachine
 
 initial_game_state = GameState(
-    players={}, # dict with `player_id: player_dict` entries
-    chaser_id=None, # id of player who is chaser
-    protection=None, # wether protection from the chaser is active
-    countdown=0.0 # countdown until protection is lifted
+    players={},  # dict with `player_id: player_dict` entries
+    chaser_id=None,  # id of player who is chaser
+    protection=None,  # wether protection from the chaser is active
+    countdown=0.0,  # countdown until protection is lifted
 )
 game_state_store = GameStateStore(initial_game_state)
 game_state_machine = GameStateMachine(game_state_store)
 
+
 def on_join(player_name, game_state, dt):
-    print('Player', player_name, 'joined.')
+    print("Player", player_name, "joined.")
     # count up for player ids, starting with 1
     player_id = max(game_state.players.keys()) + 1 if game_state.players else 1
     return {
-        'players': {
-            player_id: {
-                'name': player_name,
-                'position': (0, 0)
-            }
-        },
-        'chaser_id': player_id if game_state.chaser_id is None else game_state.chaser_id
+        "players": {player_id: {"name": player_name, "position": (0, 0)}},
+        "chaser_id": player_id if game_state.chaser_id is None else game_state.chaser_id,
     }
 
+
 def on_move(player_id, new_position, game_state, dt):
-    return {
-        'players': {
-            player_id: {
-                'position': new_position
-            }
-        }
-    }
+    return {"players": {player_id: {"position": new_position}}}
+
 
 def time_step(game_state, dt):
     # Before a player joins, updating the game state is unnecessary.
@@ -40,34 +32,28 @@ def time_step(game_state, dt):
     # If protection mode is on, all players are safe from the chaser.
     if game_state.protection:
         new_countdown = game_state.countdown - dt
-        return {
-            'countdown': new_countdown,
-            'protection': True if new_countdown >= 0.0 else False
-        }
+        return {"countdown": new_countdown, "protection": True if new_countdown >= 0.0 else False}
     # Check if chaser got someone
     chaser = game_state.players[game_state.chaser_id]
     for player_id, player in game_state.players.items():
         if not player_id == game_state.chaser_id:
             # Calculate their distance to the chaser.
-            dx = player['position'][0] - chaser['position'][0]
-            dy = player['position'][1] - chaser['position'][1]
-            distance_squared = dx*dx + dy*dy
+            dx = player["position"][0] - chaser["position"][0]
+            dy = player["position"][1] - chaser["position"][1]
+            distance_squared = dx * dx + dy * dy
             # When the chaser touches another player, that player becomes
             # chaser and the protection countdown starts.
             if distance_squared < 15:
-                print(player['name'], 'has been caught')
-                return {
-                    'chaser_id': player_id,
-                    'protection': True,
-                    'countdown': 5.0
-                }
+                print(player["name"], "has been caught")
+                return {"chaser_id": player_id, "protection": True, "countdown": 5.0}
     return {}
-        
+
+
 game_state_machine.time_step = time_step
-game_state_machine.push_event_handler(event_type='JOIN', event_handler_function=on_join)
-game_state_machine.push_event_handler(event_type='MOVE', event_handler_function=on_move)
+game_state_machine.register_event_handler(event_type="JOIN", event_handler_function=on_join)
+game_state_machine.register_event_handler(event_type="MOVE", event_handler_function=on_move)
 
 server = Server(game_state_store)
 
 game_state_machine.run_game_loop_in_thread()
-server.run(hostname='localhost', port=8080, event_wire=game_state_machine)
+server.run(hostname="localhost", port=8080, event_wire=game_state_machine)
